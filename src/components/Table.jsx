0dcecb4851aa8a7968df139d869976e2566bfe53
eliorGur <eliorgor19@gmail.com>
2024-04-06 15:33:57 +0300

    badddd

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import DataTable from 'react-data-table-component'
 import '../styling/Table.css';
+// import TableWithActions from './TableWithActions';
 
 
 export default function Table({ rows, columns }) {
@@ -22,7 +23,10 @@ export default function Table({ rows, columns }) {
         });
         setRecords(filteredRows);
     };
-    
+
+    // Use the HOC to enhance the Table component
+    const EnhancedTable = TableWithActions(Table, ['view', 'edit', 'delete'], handleView, handleEdit, handleDelete);
+
     return (
         <div className='container '>
             <div className='text-start'>
@@ -40,7 +44,7 @@ export default function Table({ rows, columns }) {
                 paginationPerPage={7}
                 style={{ borderRadius: '0.5rem' }}
             />
-
+            {/* <EnhancedTable columns={columns} data={records} /> */}
         </div>
     )
 }
