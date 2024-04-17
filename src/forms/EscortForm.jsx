243a680eca5e2fc4c1e6edc43bca0482bee88833
eliorGur <eliorgor19@gmail.com>
2024-04-17 19:07:09 +0300

    problem with dates

--- src/forms/EscortForm.jsx
+++ src/forms/EscortForm.jsx
@@ -2,43 +2,33 @@ import React, { useState, useEffect } from 'react';
 import "../styling/Form.css";
 import { FaCheck } from 'react-icons/fa';
 import { Button, Form } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import { fetchCities, fetchStreetsByCity, validateStreet } from '../tools/cities&streets';
-import { ValidPositiveNumber,ValidateId, validateHebrewletters, validateDateOfBirth, ValidCellPhoneNum } from '../tools/validations';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { fetchCities, fetchStreetsByCity, validateStreet, validateCity } from '../tools/cities&streets';
+import { ValidPositiveNumber, ValidateId, validateHebrewletters, validateDateOfBirth, ValidCellPhoneNum } from '../tools/validations';
 
 export default function EscortForm() {
 
   const navigate = useNavigate();
+  const { state } = useLocation();
+  let originEscort = state;
 
   const [cities, setCities] = useState([]);
   const [streets, setStreets] = useState([]);
   const [filteredCities, setFilteredCities] = useState([]);
 
-  const [escort, setEscort] = useState({
-    esc_firstName: '',
-    esc_lastName: '',
-    esc_id: '',
-    esc_dateofbirth: '',
-    esc_cell: '',
-    esc_city: '',
-    esc_street: '',
-    esc_homeNum: ''
-  });
+  const [escort, setEscort] = useState({ ...originEscort });
 
-  const [errors, setErrors] = useState({
-    esc_firstName: '',
-    esc_lastName: '',
-    esc_id: '',
-    esc_dateofbirth: '',
-    esc_cell: '',
-    esc_city: '',
-    esc_street: '',
-    esc_homeNum: ''
-  });
+  const [errors, setErrors] = useState({});
 
   //render the cities on-load
   useEffect(() => {
     fetchCities().then(cities => setCities(cities));
+  }, []);
+
+  useEffect(() => {
+    if (originEscort.esc_city != '') {
+      fetchStreetsByCity(originEscort.esc_city).then(streets => setStreets(streets));
+    }
   }, []);
 
   //filterout cities that dont match the search
@@ -69,16 +59,15 @@ export default function EscortForm() {
   const validateForm = () => {
     let valid = true;
     let newErrors = {};
-    console.log('escort=',escort);
-    newErrors.esc_firstName = validateHebrewletters(escort.esc_firstName);
-    newErrors.esc_lastName = validateHebrewletters(escort.esc_lastName);
+    console.log('escort=', escort);
+    newErrors.esc_fullName = validateHebrewletters(escort.esc_fullName);
     newErrors.esc_id = ValidateId(escort.esc_id);
     newErrors.esc_dateofbirth = validateDateOfBirth(escort.esc_dateofbirth);
     newErrors.esc_cell = ValidCellPhoneNum(escort.esc_cell);
-    newErrors.esc_city = validateCity(escort.esc_city,cities);
-    newErrors.esc_street = validateStreet(escort.esc_street,streets);
+    newErrors.esc_city = validateCity(escort.esc_city, cities);
+    newErrors.esc_street = validateStreet(escort.esc_street, streets);
     newErrors.esc_homeNum = ValidPositiveNumber(escort.esc_homeNum);
-    
+
     //need to check the following code *********
     setErrors(newErrors);
     console.log('errors after=', errors);
@@ -87,7 +76,7 @@ export default function EscortForm() {
         valid = false;
       }
     });
-    return false;
+    return valid;
   };
 
   return (
@@ -95,29 +84,16 @@ export default function EscortForm() {
       <div className='row' >
         <h2>הוספת מלווה</h2>
         <Form className='col-9 escortsform label-input col-form-label-sm' style={{ margin: '0 auto' }} onSubmit={handleSubmit}>
-          <Form.Group controlId="esc_firstName">
-            <Form.Label>שם פרטי</Form.Label>
-            <Form.Control type="text" name="esc_firstName"
-              value={escort.esc_firstName}
-              onChange={(e) => setEscort({ ...escort, esc_firstName: e.target.value })}
-              isInvalid={!!errors.esc_firstName}
+          <Form.Group controlId="esc_fullName">
+            <Form.Label>שם מלא</Form.Label>
+            <Form.Control type="text" name="esc_fullName"
+              value={escort.esc_fullName}
+              onChange={(e) => setEscort({ ...escort, esc_fullName: e.target.value })}
+              isInvalid={!!errors.esc_fullName}
               required
             />
             <Form.Control.Feedback type="invalid">
-              {errors.esc_firstName}
-            </Form.Control.Feedback>
-          </Form.Group>
-
-          <Form.Group controlId="esc_lastName">
-            <Form.Label>שם משפחה</Form.Label>
-            <Form.Control type="text" name="esc_lastName"
-              value={escort.esc_lastName}
-              onChange={(e) => setEscort({ ...escort, esc_lastName: e.target.value })}
-              isInvalid={!!errors.esc_lastName}
-              required
-            />
-            <Form.Control.Feedback type="invalid">
-              {errors.esc_lastName}
+              {errors.esc_fullName}
             </Form.Control.Feedback>
           </Form.Group>
 
@@ -149,12 +125,12 @@ export default function EscortForm() {
 
           <Form.Group controlId="esc_cell">
             <Form.Label>נייד</Form.Label>
-            <Form.Control type="text" name="esc_cell" 
-             value={escort.esc_cell}
-             onChange={(e) => setEscort({ ...escort, esc_cell: e.target.value })}
-             isInvalid={!!errors.esc_cell}
-             required
-           />
+            <Form.Control type="text" name="esc_cell"
+              value={escort.esc_cell}
+              onChange={(e) => setEscort({ ...escort, esc_cell: e.target.value })}
+              isInvalid={!!errors.esc_cell}
+              required
+            />
             <Form.Control.Feedback type="invalid">
               {errors.esc_cell}
             </Form.Control.Feedback>
@@ -185,15 +161,15 @@ export default function EscortForm() {
           <Form.Group controlId="esc_street">
             <Form.Label>רחוב</Form.Label>
             <div className="select-container">
-            <Form.Control className='formSelect' as="select" name="esc_street"
-              value={escort.esc_street}
-              onChange={(e) => setEscort({ ...escort, esc_street: e.target.value })}
-              isInvalid={!!errors.esc_street}
-              required >
-              {streets.map((street, index) => (
-                <option key={index} value={street}>{street}</option>
-              ))}
-            </Form.Control>
+              <Form.Control className='formSelect' as="select" name="esc_street"
+                value={escort.esc_street}
+                onChange={(e) => setEscort({ ...escort, esc_street: e.target.value })}
+                isInvalid={!!errors.esc_street}
+                required >
+                {streets.map((street, index) => (
+                  <option key={index} value={street}>{street}</option>
+                ))}
+              </Form.Control>
             </div>
             <Form.Control.Feedback type="invalid">
               {errors.esc_street}
@@ -213,12 +189,12 @@ export default function EscortForm() {
             </Form.Control.Feedback>
           </Form.Group>
           <div className='text-center' style={{ paddingTop: '20px' }}>
-        <Button  type="submit" >שמור <FaCheck style={{ paddingBottom: '2px' }} /></Button>
-      </div>
-        </Form>        
+            <Button type="submit" >שמור <FaCheck style={{ paddingBottom: '2px' }} /></Button>
+          </div>
+        </Form>
       </div>
 
-      
+
     </div>
   );
 }
--- src/pages/Escorts.jsx
+++ src/pages/Escorts.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from '../components/Table';
 import { Button } from 'react-bootstrap';
 import { FaPlus } from "react-icons/fa6";
-import {useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 
@@ -11,31 +11,107 @@ export default function Escorts() {
   const navigate = useNavigate();
 
   const addEscort = () => {
-    navigate('/EscortForm');
+    let newEscort = {
+      esc_fullName: '',
+      esc_id: '',
+      esc_dateOfBirth: '',
+      esc_cell: '',
+      esc_city: '',
+      esc_street: '',
+      esc_homeNum: ''
+    };
+    navigate('/EscortForm', { state: newEscort });
   }
-  
 
-//3 functions that handle viewing, editing and deleting a row
-const handleView = (row) => {
-  console.log('View:', row);
-  // Add your view logic here
-};
+  const escortColumns = [
+    {
+      name: "שם מלא",
+      selector: (row) => row.fullName,
+      sortable: true,
+    },
+    {
+      name: "תעודת זהות",
+      selector: (row) => row.id,
+    },
+    {
+      name: "נייד",
+      selector: (row) => row.cell,
+    },
+    {
+      name: "כתובת",
+      selector: (row) => row.street + ' ' + row.homeNum + ', ' + row.City,
+      sortable: true,
+    },
+    {
+      name: "תאריך לידה",
+      selector: (row) => row.dateOfBirth,
+      sortable: true,
+    },
+  ];
 
-const handleEdit = (row) => {
-  console.log('Edit:', row);
-  // Add your edit logic here
-};
+  const escortRows = [
+    //GET
+    {
+      fullName: 14,
+      id: "אברהמסון",
+      street: 'ברנר',
+      homeNum: 5,
+      City: 'כפר סבא',
+      dateOfBirth: '05/10/1999',
+      cell: '0527458899'
+    },
+    {
+      fullName: 7,
+      id: "אהרוני",
+      street: 'ויצמן',
+      homeNum: 8,
+      City: 'כפר סבא',
+      dateOfBirth: '10/02/1997',
+      cell: '0527458844'
+    },
+    {
+      fullName: 80,
+      id: "השרונים",
+      street: 'הנביאים',
+      homeNum: 85,
+      City: 'כפר סבא',
+      dateOfBirth: '01/04/2010',
+      cell: '052745877'
+    },
+  ];
 
-const handleDelete = (row) => {
-  console.log('Delete:', row);
-  // Add your delete logic here
-};
+
+  //3 functions that handle viewing, editing and deleting a row
+  const handleView = (row) => {
+    console.log('View:', row);
+    // Add your view logic here
+  };
+
+  const handleEdit = (row) => {
+    console.log('Edit:', row);
+    // Add your edit logic here
+    let currentEscort = {
+      esc_fullName: row.fullName,
+      esc_id: row.id,
+      esc_dateOfBirth: row.dateOfBirth,
+      esc_cell: row.cell,
+      esc_city: row.City,
+      esc_street: row.street,
+      esc_homeNum: row.homeNum,
+    };
+    navigate('/EscortForm', { state: currentEscort });
+  };
+
+  const handleDelete = (row) => {
+    console.log('Delete:', row);
+    // Add your delete logic here
+  };
 
 
   return (
     <div className='container mt-5' >
       <h3 className="bold" style={{ textAlign: 'center' }}>נתוני מלווים</h3>
-      <Table columns={""} rows={""} handleView={handleView} handleEdit={handleEdit} handleDelete={handleDelete} />
+      <Table columns={escortColumns} rows={escortRows} handleView={handleView} handleEdit={handleEdit} handleDelete={handleDelete} />
       <div className='text-center'
         style={{ padding: '20px' }}>
         <Button onClick={addEscort}>הוסף מלווה חדש <FaPlus style={{ paddingBottom: '2px' }} /></Button></div>
