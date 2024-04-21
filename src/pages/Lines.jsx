9c0cc880c645b7c5cc3c81cdb8dbc4ef6a02e1ea
bararad <bararad@gmail.com>
2024-04-21 14:16:17 +0300

    fixed line+times

--- src/pages/Lines.jsx
+++ src/pages/Lines.jsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 import Table from '../components/Table';
 import { Button } from 'react-bootstrap';
 import { FaPlus } from "react-icons/fa6";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate,useLocation} from 'react-router-dom';
 import MyModal from '../components/MyModal';
 
 
 export default function Lines() {
 
   const navigate = useNavigate();
+  const { state } = useLocation();
+  let addedLine = state;
+
   const [showModal, setShowModal] = useState(false);
   const [rowData, setRowData] = useState(null);
   const [colData, setColData] = useState(null);
@@ -26,7 +29,8 @@ export default function Lines() {
       line_city: "",
       line_street: "",
       line_Homenumber: "",
-      time_of_line: ""
+      time_of_line: "",
+      definition_date:"",
     }
     navigate('/LineForm', { state: newLine });
   }
@@ -67,14 +71,14 @@ export default function Lines() {
       line_code: 1,
       line_car: "אוטובוס",
       number_of_seats: 3,
-      escort_incharge: "בנימין בן בוי",
+      escort_incharge: "בן בוי",
       school_of_line: "טשרני",
       station_definition: "מוצא",
       time_of_line: "19:17",
       line_city: "נתניה",
       line_street: "שד' בנימין",
       line_Homenumber: "3",
-      definition_date: "12.04.2021"
+      definition_date: "12/04/2021"
     },
     {
       line_code: "2",
@@ -91,6 +95,8 @@ export default function Lines() {
     },
   ];
 
+  const updatedLineRows = addedLine ? [...Linerows, addedLine] : Linerows;
+  console.log("updatedLineRows",updatedLineRows);
   //define the hebrew names of each field
   const ColumnNamesByIdentifier =
   {
@@ -140,11 +146,10 @@ export default function Lines() {
     // Add your delete logic here
   };
 
-
   return (
     <div className='container mt-5' >
       <h3 className="bold" style={{ textAlign: 'center' }}>קווי הסעה</h3>
-      <Table columns={Linecolumns} rows={Linerows} handleView={handleView} handleEdit={handleEdit} handleDelete={handleDelete} />
+      <Table columns={Linecolumns} rows={updatedLineRows} handleView={handleView} handleEdit={handleEdit} handleDelete={handleDelete} />
       <div className='text-center'
         style={{ padding: '20px' }}>
         <Button onClick={addLine}>הוסף קו חדש <FaPlus style={{ paddingBottom: '2px' }} /></Button></div>
