5b25c5fbde535ba4a53c356e860ffe3d665b0593
bararad <bararad@gmail.com>
2024-04-26 14:36:44 +0300

    Update cities&streets.js

--- src/tools/cities&streets.js
+++ src/tools/cities&streets.js
@@ -57,11 +57,7 @@ export const validateCity = (input,cities) => {
         return '';
     }
 };
-<<<<<<< Updated upstream
-export const validateStreet = (input,streets) => {    
-=======
 export const validateStreet = (input,streets) => {
->>>>>>> Stashed changes
     let chosenstreet = input;
     console.log("chosenstreet",chosenstreet);
     console.log("streets",streets);
