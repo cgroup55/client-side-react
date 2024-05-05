9eaac69c44c3e9ce76443bb56b8d369b6f68f699
eliorGur <eliorgor19@gmail.com>
2024-05-05 20:14:47 +0300

    login handle

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,76 +1,63 @@
-import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';;
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import { styled } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import Swal from 'sweetalert2';
-
-const BootstrapButton = styled(Button)({
-    boxShadow: 'none',
-    textTransform: 'none',
-    fontSize: 16,
-    padding: '6px 12px',
-    border: '1px solid',
-    lineHeight: 1.5,
-    backgroundColor: '#4B8443',
-    borderColor: '#4B8443',
-
-    '&:hover': {
-        backgroundColor: '#2E4D11',
-        borderColor: '#2E4D11',
-    },
-    '&:active': {
-        backgroundColor: '#2E4D11',
-        borderColor: '#2E4D11',
-    },
-    '&:focus': {
-        boxShadow: '0 0 0 0.2rem rgba(46,77,17,.5)',
-    },
-});
+import { create } from "../tools/api";
 
 export default function Login() {
 
+    const url = 'api/User/Login';
     const [user, setUser] = useState({});
 
-
-
     const navigate = useNavigate();
-    const redirectTo = (user) => {
+
+    const checkUser = async (userToCheck) => {
+        console.log('userToCheck:', userToCheck);
+        let res = await create(url, userToCheck);
+        if (res == undefined || res == null) {
+            console.log('שגיאה- יוזר לא קיים');
+            Swal.fire({
+                title: "קיימת תקלה",
+                text: "אנא נסה שנית במועד מאוחר יותר",
+                icon: "error"
+            });
+            return;
+        }
+        return res;
+        //AD HOC from DB for Parent- all his children Escort- all his lines
+    }
+
+    const redirectTo = async (event) => {
         //POST user to pass on to dbs
         //check if the user that we got is a listed user
         //return from fetch
         //fetch("POST",user....)
         //return()
+        event.preventDefault();
 
-        //erase after dbs connection
-        navigate('/homepage');
-        if (user == undefined) {
+
+        let userFromDB = await checkUser(user);
+        console.log('userFromDB:', userFromDB);
+        if (userFromDB.role == 1) {
+            //need to add check if user exists+take user login info***
             Swal.fire({
-                title: "נסה שנית",
-                text: "פרטייך שגויים",
+                title: "התחברת בהצלחה",
+                text: "שלום משרד!",
+                icon: "success"
+            });
+            navigate('/homepage');
+        }
+        //need to change the logic for role 1 or 2 
+        else {
+            Swal.fire({
+                title: "לא נמצא משתמש עם פרטים אלה במערכת",
+                text: "נסה להזין שנית",
                 icon: "error"
             });
         }
-        else 
-        {
-            if (user.role == 0) {
-                //need to add check if user exists+take user login info***
-                Swal.fire({
-                    title: "נכנסת בהצלחה",
-                    text: "שלום משרד!",
-                    icon: "success"
-                });
-                //navigate('/homepage');
-            }
-            else {
-                Swal.fire({
-                    title: "פרטים שגויים",
-                    text: "היכנס לאפליקציה הייעודית",
-                    icon: "error"
-                });
-            }
 
-        }
+
     }
 
 
@@ -90,25 +77,25 @@ export default function Login() {
                 <img src="../src/images/clearlogo.png" style={{ width: "300px", margin: "20px", marginBottom: "30px" }} alt="" />
                 <h3 style={{ textAlign: 'center' }}>ברוכים הבאים!</h3>
                 <br />
-                <Form>
+                <Form onSubmit={redirectTo}>
                     <Form.Group className="md-3" controlId="formBasicEmail">
                         <Form.Label>שם משתמש:</Form.Label>
                         <Form.Control type="text"
                             onChange={(e) => { setUser({ ...user, userName: e.target.value }) }}
-                        />
+                            required />
                     </Form.Group>
 
                     <Form.Group className="md-3" controlId="formBasicPassword">
                         <Form.Label>סיסמא:</Form.Label>
                         <Form.Control type="password"
                             onChange={(e) => { setUser({ ...user, password: e.target.value }) }}
-                        />
+                            required />
                     </Form.Group>
 
                     <div style={{ display: 'flex', justifyContent: 'center', padding: 20 }}>
-                        <BootstrapButton onClick={redirectTo}>
+                        <Button type='submit'>
                             התחבר
-                        </BootstrapButton>
+                        </Button>
                     </div>
                 </Form>
             </div>
